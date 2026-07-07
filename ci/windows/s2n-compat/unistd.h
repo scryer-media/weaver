f5d18d8eeee8cda496d97186292b5390d94bf3dd
NZB Man <133935777+NZBMan@users.noreply.github.com>
2026-07-07 12:39:27 -0600

    s2n windows fixes

--- ci/windows/s2n-compat/unistd.h
+++ ci/windows/s2n-compat/unistd.h
@@ -6,14 +6,6 @@
 #include <sys/stat.h>
 #include <time.h>
 
-#ifndef _TIMESPEC_DEFINED
-#define _TIMESPEC_DEFINED
-struct timespec {
-    time_t tv_sec;
-    long tv_nsec;
-};
-#endif
-
 __declspec(dllimport) void __stdcall Sleep(unsigned long dwMilliseconds);
 
 #ifndef fstat
